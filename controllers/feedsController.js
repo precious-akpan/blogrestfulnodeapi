94e05024cd6254db5e0ae0187c4a2daeaff9b9c2
Precious Akpan <precious.akpan2000@gmail.com>
2023-08-11 16:42:00 +0100

    added rest api endpoints

--- controllers/feedsController.js
+++ controllers/feedsController.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const { join } = require("path");
 const { validationResult } = require("express-validator");
 const Post = require("../models/posts");
+const User = require("../models/user");
 const clearImage = (filePath) => {
   fs.unlink(join(__dirname, "..", filePath), (error) => {
     console.log(error);
@@ -44,6 +45,7 @@ exports.createPost = (req, res, next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   if (!req.file) {
     const error = new Error("No image provided!");
     error.statusCode = 422;
@@ -54,14 +56,22 @@ exports.createPost = (req, res, next) => {
     title,
     content,
     imageUrl: imageUrl,
-    creator: { name: "Christopher" },
+    creator: req.userId,
   });
   return post
     .save()
-    .then((r) => {
+    .then((r) => User.findOne({ _id: req.userId }))
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
+      console.log("created post", post);
       res.status(201).json({
         message: "Post created successfully!",
-        post: r,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((error) => {
@@ -72,24 +82,25 @@ exports.createPost = (req, res, next) => {
     });
 };
 
-exports.getPost = (req, res, next) => {
+exports.getPost = async (req, res, next) => {
   const { postId } = req.params;
-  Post.findById(postId)
-    .then((post) => {
-      if (!post) {
-        const error = new Error("Post not found");
-        error.statusCode = 404;
-        throw error;
-      }
+  try {
+    const post = await Post.findById(postId);
+    // .then((post) => {
+    if (!post) {
+      const error = new Error("Post not found");
+      error.statusCode = 404;
+      throw error;
+    }
 
-      res.status(200).json({ message: "Pose fetched", post });
-    })
-    .catch((error) => {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
-    });
+    res.status(200).json({ message: "Pose fetched", post });
+    // })
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
 exports.updatePost = (req, res, next) => {
@@ -121,6 +132,11 @@ exports.updatePost = (req, res, next) => {
       if (post.imageUrl !== imageUrl) {
         clearImage(post.imageUrl);
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Unauthorized edit request");
+        error.statusCode = 403;
+        throw error;
+      }
       post.title = title;
       post.content = content;
       post.imageUrl = imageUrl;
@@ -146,11 +162,23 @@ exports.deletePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Unauthorized request");
+        error.statusCode = 403;
+        throw error;
+      }
       clearImage(post.imageUrl);
-      return Post.findByIdAndDelete(postId);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(() => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      console.log(user);
+      user.posts.pull(postId);
+      return user.save();
     })
     .then((result) => {
-      console.log(result);
       res.status(200).json({ message: "Deleted post" });
     })
     .catch((error) => {
